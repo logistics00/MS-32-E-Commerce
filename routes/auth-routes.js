f076f68f5689c973b13630b64c29e9df1ae8f7df
Niek Mollers <info@sofane.nl>
2022-01-17 01:21:43 +0100

    543 Adding A MongoDB Database Server & Connection

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -5,6 +5,7 @@ const authControllers = require('../controllers/auth-controller');
 const router = express.Router();
 
 router.get('/signup', authControllers.getSignup);
+router.post('/signup', authControllers.signup);
 router.get('/login', authControllers.getLogin);
 
 module.exports = router;
