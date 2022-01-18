3cac8ba0e959762ebc55b3e8d47274b5dd94cd11
Niek Mollers <info@sofane.nl>
2022-01-18 02:10:57 +0100

    547 Introducing Sessions & Configuring Sessions

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const path = require('path');
 
 const express = require('express');
 const csrf = require('csurf');
+const expressSession = require('express-session');
 
+const createSessionConfig = require('./config/session');
 const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
@@ -18,6 +20,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
 app.use(csrf());
 app.use(addCsrfTokenMiddleware);
 
