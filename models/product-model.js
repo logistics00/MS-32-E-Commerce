d8b32d5ac22a24fe92dae569a846d12b06dcacbe
logistics <info@sofane.nl>
2022-01-22 03:55:42 +0100

    565 Updating Products (As Administrator)

--- models/product-model.js
+++ models/product-model.js
@@ -9,8 +9,7 @@ class Product {
     this.price = +productData.price;
     this.description = productData.description;
     this.image = productData.image;
-    this.imagePath = `product-data/images/${productData.image}`;
-    this.imageUrl = `/products/assets/images/${productData.image}`;
+    this.updateImageData();
     if (productData._id) {
       this.id = productData._id.toString(); //,
     }
@@ -36,7 +35,7 @@ class Product {
       throw error;
     }
 
-    return product;
+    return new Product(product);
   }
 
   static async findAll() {
@@ -49,7 +48,12 @@ class Product {
     });
   }
 
-  async save() {
+  updateImageData() {
+    this.imagePath = `product-data/images/${this.image}`;
+    this.imageUrl = `/products/assets/images/${this.image}`;
+  }
+
+  async save(image) {
     const productData = {
       title: this.title,
       summary: this.summary,
@@ -57,7 +61,28 @@ class Product {
       description: this.description,
       image: this.image,
     };
-    await db.getDb().collection('products').insertOne(productData);
+
+    if (this.id) {
+      const prodId = new mongodb.ObjectId(this.id);
+
+      if (!this.image) {
+        delete productData.image;
+      }
+
+      await db.getDb().collection('products').updateOne(
+        { _id: prodId },
+        {
+          $set: productData,
+        }
+      );
+    } else {
+      await db.getDb().collection('products').insertOne(productData);
+    }
+  }
+
+  async replaceImage(newImage) {
+    this.image = newImage;
+    this.updateImageData();
   }
 }
 
