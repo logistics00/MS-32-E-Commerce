48ec8f04d30a5e473230585af495d9b270fc49a1
logistics <info@sofane.nl>
2022-01-24 23:54:09 +0100

    581 Adding The Order Controller & A Basic Order Model

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const baseRoutes = require('./routes/base-routes');
 const productsRoutes = require('./routes/products-routes');
 const adminRoutes = require('./routes/admin-routes');
 const cartRoutes = require('./routes/cart-routes');
+const ordersRoutes = require('./routes/orders-routes');
 
 const app = express();
 
@@ -45,6 +46,7 @@ app.use(authRoutes);
 app.use(productsRoutes);
 app.use('/cart', cartRoutes);
 app.use(protectRoutesMiddleware);
+app.use('/orders', ordersRoutes);
 app.use('/admin', adminRoutes);
 
 app.use(errorHandlerMiddleware);
