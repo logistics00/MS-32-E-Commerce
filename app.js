7fedcbdc4cd7a98d5e614fafa0e8ffe61794059e
logistics <info@sofane.nl>
2022-01-25 03:31:35 +0100

    586 Bugfixing & Polishing

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const checkAuthStatusMiddleware = require('./middlewares/check-auth');
 const protectRoutesMiddleware = require('./middlewares/protect-routes');
 const cartMiddleware = require('./middlewares/cart');
 const updateCartPricesMiddleware = require('./middlewares/update-cart-prices');
+const notFoundMiddleware = require('./middlewares/not-found');
 const authRoutes = require('./routes/auth-routes');
 const baseRoutes = require('./routes/base-routes');
 const productsRoutes = require('./routes/products-routes');
@@ -46,9 +47,10 @@ app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
 app.use('/cart', cartRoutes);
-app.use(protectRoutesMiddleware);
-app.use('/orders', ordersRoutes);
-app.use('/admin', adminRoutes);
+app.use('/orders', protectRoutesMiddleware, ordersRoutes);
+app.use('/admin', protectRoutesMiddleware, adminRoutes);
+
+app.use(notFoundMiddleware);
 
 app.use(errorHandlerMiddleware);
 
